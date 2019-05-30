59414e327b3ae91c44b20fad283641d6ae771c0a
Ruslan Volyar <rvolyar@gmail.com>
2019-05-30 01:40:56 -0400

    init

---
+++ constants/Colors.js
@@ -0,0 +1,18 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  headerColor: "#47525E",
+  mainButton: "#0095E4",
+  mainButtonActive: "#0077B6",
+  secondaryButtonActive: "#374049"
+};
